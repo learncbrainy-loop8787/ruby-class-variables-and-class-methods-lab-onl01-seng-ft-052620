705d264e6f95875ad98d70984058947fe38a5d9b
korey ko <koreykendrick1@gmail.com>
2020-06-04 19:07:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,27 @@ class Song
     @@count 
   end 
 
+GENRE = []
  
- GENRE = []
- ARTIST = []
+ def self.genre 
+   @@genre.uniq 
+ end 
+ def self.genre_count 
+   @@genre.inject(hash.new(0)) { |total, i| total[i] +=1 :total}
+ end
+ 
+ def self.genre_count
+   genre_count = {} 
+   @@genre.each do |genre| 
+     if genre_count[genre] += 1 
+     else 
+       genre_count[genre] = 1 
+     end 
+   end 
+   genre_count 
+ end
+       
+       genre_count[genre]
   def initialize(name, artist, genre)
     @@name += 1
     @@artist += 1 
